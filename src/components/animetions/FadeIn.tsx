d5a6a88b0f0ca0373d1d07f15d39399d16b4bdad
seiyaookura <seiya.ookura@gmail.com>
2024-02-16 21:47:00 +0900

    2024/02/16 ・TypeScriptファイル追加

---
+++ src/components/animetions/FadeIn.tsx
@@ -0,0 +1,21 @@
+// import { motion } from 'framer-motion';
+// import { Header } from '../common/Header';
+// import { Footer } from '../common/Footer';
+// import { ChildrenType } from '../utility/type/ChildrenType';
+
+// export const FadeIn = (props: ChildrenType) => {
+//   return (
+//     <>
+//       <motion.div
+//         className=""
+//         initial={{ opacity: 0, y: 0 }}
+//         animate={{ opacity: 1, y: 0 }}
+//         transition={{ duration: 2.5 }}
+//       >
+//         <Header />
+//         {props.children}
+//         <Footer />
+//       </motion.div>
+//     </>
+//   );
+// };
