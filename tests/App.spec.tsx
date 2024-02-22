0df9eb82159de3728c8ce6668f3b608d174bca02
seiyaookura <seiya.ookura@gmail.com>
2024-02-22 10:09:26 +0900

    2024/02/22 ・jest導入

---
+++ tests/App.spec.tsx
@@ -0,0 +1,9 @@
+// import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import '@testing-library/jest-dom';
+
+// const consoleLogMock = jest.spyOn(console, 'log');
+
+test('デモテスト', () => {
+  expect(true).toBe(true);
+});
