5018bd4e58960d2593846cf692952365cbaf35ff
せいーや <79490150+ookuraseiya@users.noreply.github.com>
2026-02-16 13:56:50 +0900

    Disable microCMS functionality in Personal component
    Comment out code related to microCMS and data fetching.

--- src/pages/Personal.tsx
+++ src/pages/Personal.tsx
@@ -1,42 +1,44 @@
 import { Footer } from '../layouts/Footer/Footer';
 import { Header } from '../layouts/Header/Header';
-import { Pagination } from '../layouts/Pagination/Pagination';
-import { PersonalType } from '../types/PersonalType';
-import { Loading } from '../components/Animation/Loading';
-import { isURLEnabled } from '../features/isURLEnabled';
-import { useFetchPostsData } from '../hooks/useFetchPostsData';
-import { getPaginationInfo } from '../features/getPaginationInfo';
-import {
-  ACQUISITION_CONDITION,
-  API,
-  DOMAIN,
-  PERSONAL_END_POINT,
-} from '../config/config';
-import { useGetCurrentPageId } from '../hooks/useGetCurrentPageId';
-import { isPostsDataExisted } from '../features/isPostsDataExisted';
-import { PersonalItem } from '../layouts/PersonalItem/PersonalItem';
+// microCMSで個人開発ページを削除したので、一時的な応急処置
+// import { Pagination } from '../layouts/Pagination/Pagination';
+// import { PersonalType } from '../types/PersonalType';
+// import { Loading } from '../components/Animation/Loading';
+// import { isURLEnabled } from '../features/isURLEnabled';
+// import { useFetchPostsData } from '../hooks/useFetchPostsData';
+// import { getPaginationInfo } from '../features/getPaginationInfo';
+// import {
+//   ACQUISITION_CONDITION,
+//   API,
+//   DOMAIN,
+//   PERSONAL_END_POINT,
+// } from '../config/config';
+// import { useGetCurrentPageId } from '../hooks/useGetCurrentPageId';
+// import { isPostsDataExisted } from '../features/isPostsDataExisted';
+// import { PersonalItem } from '../layouts/PersonalItem/PersonalItem';
 
 export const Personal = () => {
-  const currentPageId = useGetCurrentPageId();
+  // microCMSで個人開発ページを削除したので、一時的な応急処置
+  // const currentPageId = useGetCurrentPageId();
 
-  const { postsData, isLoading } = useFetchPostsData<PersonalType>(
-    DOMAIN,
-    PERSONAL_END_POINT,
-    ACQUISITION_CONDITION,
-    API,
-    currentPageId
-  );
+  // const { postsData, isLoading } = useFetchPostsData<PersonalType>(
+  //   DOMAIN,
+  //   PERSONAL_END_POINT,
+  //   ACQUISITION_CONDITION,
+  //   API,
+  //   currentPageId
+  // );
 
-  const { lastPost, firstPost, paginationNumber } = getPaginationInfo(
-    currentPageId,
-    postsData
-  );
+  // const { lastPost, firstPost, paginationNumber } = getPaginationInfo(
+  //   currentPageId,
+  //   postsData
+  // );
 
   return (
-    <>
-      {isLoading ? (
-        <Loading />
-      ) : (
+    // <>
+    //   {isLoading ? (
+    //     <Loading />
+    //   ) : (
         <>
           <Header />
           <section className="personal">
@@ -45,8 +47,7 @@ export const Personal = () => {
               <p className="personal__lead">
                 これまで個人で開発してきたプロダクトです
               </p>
-              {/* microCMSで個人開発ページを削除したので、一時的な応急処置 */}
-              /* {isURLEnabled(currentPageId, paginationNumber) &&
+              {/* {isURLEnabled(currentPageId, paginationNumber) &&
               isPostsDataExisted<PersonalType>(postsData) ? (
                 <>
                   <article className="personal__wrapper">
@@ -64,7 +65,7 @@ export const Personal = () => {
                 <h1 className="personal__card--error">
                   プロダクトがありません。
                 </h1>
-              )} */
+              )} */}
                 <h1 className="personal__card--error">
                   プロダクトがありません。
                 </h1>
@@ -72,7 +73,7 @@ export const Personal = () => {
           </section>
           <Footer />
         </>
-      )}
-    </>
+    //   )}
+    // </>
   );
 };
